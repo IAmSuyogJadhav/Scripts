e8d83627d5152b3a77146d109d7204ffce6a5029
Suyog Jadhav <suyog.jadhav1@gmail.com>
2019-07-08 16:14:26 +0530

    Update utils.py

--- BioMed/utils.py
+++ BioMed/utils.py
@@ -18,30 +18,44 @@ def read_labels(labels_path):
     """
     For reading in labels of patches from ProstateX challenge.
     """
-    labels = pd.read_excel(labels_path)
+    labels = pd.read_csv(labels_path)
+    labels = labels[['ProxID', 'Name', 'coord']]
     labels.dropna(inplace=True)
 
-    labels.ProxID = labels.ProxID.map(lambda x: x[-4:])
-    labels.Name = labels.Name.map(lambda x: 'ADC' if 'ADC' in x else ('BVAL' if 'BVAL' in x else None))
-    labels['coord'] = labels['Patch-Locations'].map(lambda x: x.split()).map(lambda x: list(map(int, x)))
-    labels['title'] = labels.ProxID + '-' + labels.Name
-
-    labels = labels[['title', 'coord']]
+    labels['ID'] = labels.ProxID.map(lambda x: int(x[-4:]))
+    labels['modality'] = labels.Name.map(lambda x: 'sag' if 'sag' in x else None)
+    labels['coords'] = labels['coord'].map(lambda x: x.split()).map(lambda x: list(map(int, x)))
+    labels = labels[['ID', 'modality', 'coords']]
     labels.dropna(inplace=True)
-    labels.set_index('title', inplace=True)
     return labels
+
+
+def get_coords(id, labels=None):
+    """
+    Get ground truth coordinates for given training example ID.
+    For use with ProstateX Challenge Dataset.
+    """
+    if labels is None:
+        labels = read_labels(
+            '/home/user/prostatechallenge/ProstateX2-DataInfo-Train/ProstateX-2-Images-Train.csv'
+            )
+    labels.set_index('ID')
+    groups = labels.groupby('ID').groups
+    return list(labels.coords[groups[2]])
 
 
 def enlarge_label(img, coord, r=1):
     """
-    Expects shape to be (2, depth, width, height)
+    Expects shape to be (modalities, depth, width, height)
     outputs segmentation of shape (1, depth, width, height)
     """
     seg = np.zeros_like(img[0]).astype(np.uint8)
-    frame = seg[coord[2] - 1, ...]
-    frame = cv2.rectangle(
-        frame, (coord[0]-1, coord[1]+1), (coord[0]+1, coord[1]-1), 255, -1)
-    seg[coord[2] - 1, ...] = frame
+
+    for coord in coords:
+        frame = seg[coord[2] - 1, ...]
+        frame = cv2.rectangle(
+            frame, (coord[0]-1, coord[1]+1), (coord[0]+1, coord[1]-1), 1, -1)
+        seg[coord[2] - 1, ...] = frame
     return seg
 
 
@@ -56,21 +70,22 @@ def save_nii(files, names, dir="./saved_nii"):
         writer.Execute(sitk.GetImageFromArray(file))
 
 
-def get_last_state(models_dir):
+def get_last_state(models_dir, model='Model'):
     r"""
     Gives out the path to the last model and its epoch number.
-    Expect models to be named as (regex): Model-.*Epoch-\d*\.h5
+    Expect models to be named as (regex): {model}-.*Epoch-\d*\.h5
+    where {model} is the prefix of your filename, defaults to Model
     """
-    models = glob.glob(f'{models_dir}/Model-.*.h5')
+    models = glob.glob(f'{models_dir}/{model}-.*.h5')
 
     if models:
-        pat = re.compile(r'.*/Model-.*Epoch-(\d*)\.h5')
-        last_model = sorted(models, reverse=True, key=lambda model: int(pat.findall(model)[0]))[0]
+        pat = re.compile(f'.*/{model}-.*Epoch-(\\d*)\\.h5')
+        last_model = sorted(models, reverse=True, key=lambda m: int(pat.findall(m)[0]))[0]
         return last_model, int(pat.findall(last_model)[0])
     else:
-        return "/gdrive/My Drive/brainy/unet/Model-train_dice={loss:.3f}-val_dice={val_loss:.3f}-Epoch-{epoch}.h5", 1
+        return f"{models_dir}/{model}-train_dice={loss:.3f}-val_dice={val_loss:.3f}-Epoch-{epoch}.h5", 1
 
-
+    
 def save_data_file(data, labels, h5_file):
     """
     For generating the data_file.h5 to be used with ellisdg/3DUnetCNN.
@@ -105,7 +120,9 @@ def flip_ud(img):
 
 # The following function needs to be worked on before applying to MRI images
 def elastic_transform(images, alpha, sigma, random_state=None):
-    """Elastic deformation of images as described in [Simard2003]_.
+    """
+    ***Might not work corectly for lower resolution images***
+    Elastic deformation of images as described in [Simard2003]_.
     .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
        Convolutional Neural Networks applied to Visual Document Analysis", in
        Proc. of the International Conference on Document Analysis and
